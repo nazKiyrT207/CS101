fcd60df06bcefd45be83cd5882e40f185bd2358d
nazKiyrT207 <100958626+nazKiyrT207@users.noreply.github.com>
2022-03-04 14:38:18 -0600

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -1,12 +1,7 @@
-def locate_substring(dna_snippet, dna):
-    start = 0
-    li=[]
-    while True: 
-        if dna.find(dna_snippet, start)==-1:
-            break
-        else:
-            start=dna.find(dna_snippet, start)
-            li.append(start)
-            start += 1 
-    return li
-print(locate_substring("ATAT","GATATATGCATATACTT"))
+def hamming_dist(dna1,dna2):
+    diff=0
+    for i in range(len(dna1)):
+        if dna1[i]!=dna2[i]:
+            diff+=1
+    return diff
+hamming_dist('GAGCCTACTAACGGGAT','CATCGTAATGACGGCCT')
