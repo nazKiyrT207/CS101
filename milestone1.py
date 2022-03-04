4310630ded002fbb1fc9256fafaddfe9c0d70301
nazKiyrT207 <100958626+nazKiyrT207@users.noreply.github.com>
2022-03-04 15:04:54 -0600

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -9,15 +9,16 @@ def rna2codon(rna):
           "UCG" : "S", "CCG": "P", "ACG": "T", "GCG": "A",
           "UAU" : "Y", "CAU": "H", "AAU": "N", "GAU": "D",
           "UAC" : "Y", "CAC": "H", "AAC": "N", "GAC": "D",
-       "UAA" : "Stop", "CAA": "Q", "AAA": "K", "GAA": "E",
-       "UAG" : "Stop", "CAG": "Q", "AAG": "K", "GAG": "E",
+       "UAA" : "", "CAA": "Q", "AAA": "K", "GAA": "E",
+       "UAG" : "", "CAG": "Q", "AAG": "K", "GAG": "E",
           "UGU" : "C", "CGU": "R", "AGU": "S", "GGU": "G",
           "UGC" : "C", "CGC": "R", "AGC": "S", "GGC": "G",
-       "UGA" : "Stop", "CGA": "R", "AGA": "R", "GGA": "G",
+       "UGA" : "", "CGA": "R", "AGA": "R", "GGA": "G",
           "UGG" : "W", "CGG": "R", "AGG": "R", "GGG": "G"}
     result = ""
     for i in range(0,len(rna),3):
         triplet = str(rna[i] + rna[i+1] + rna[i+2])
+        print(triplet)
         if triplet in genetic_code:
             result = result + genetic_code[triplet]
     return result
