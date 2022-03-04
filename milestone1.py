396b8ae4bbd86e6a9b76d91f6a6ec726928b6505
nazKiyrT207 <100958626+nazKiyrT207@users.noreply.github.com>
2022-03-04 15:27:54 -0600

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -9,22 +9,17 @@ def rna2codon(rna):
           "UCG" : "S", "CCG": "P", "ACG": "T", "GCG": "A",
           "UAU" : "Y", "CAU": "H", "AAU": "N", "GAU": "D",
           "UAC" : "Y", "CAC": "H", "AAC": "N", "GAC": "D",
-       "UAA" : "", "CAA": "Q", "AAA": "K", "GAA": "E",
-       "UAG" : "", "CAG": "Q", "AAG": "K", "GAG": "E",
+       "UAA" : "Stop", "CAA": "Q", "AAA": "K", "GAA": "E",
+       "UAG" : "Stop", "CAG": "Q", "AAG": "K", "GAG": "E",
           "UGU" : "C", "CGU": "R", "AGU": "S", "GGU": "G",
           "UGC" : "C", "CGC": "R", "AGC": "S", "GGC": "G",
-       "UGA" : "", "CGA": "R", "AGA": "R", "GGA": "G",
+       "UGA" : "Stop", "CGA": "R", "AGA": "R", "GGA": "G",
           "UGG" : "W", "CGG": "R", "AGG": "R", "GGG": "G"}
     result = ""
-    print(rna)
-    count=0
     for i in range(0,len(rna),3):
-        count+=3
-        if count>len(rna):
-            break
         triplet = str(rna[i] + rna[i+1] + rna[i+2])
-        print(triplet)
         if triplet in genetic_code:
-            result = result + genetic_code[triplet]
+            if genetic_code[triplet]=='Stop':
+                break
+        result = result + genetic_code[triplet]
     return result
-print(rna2codon('AUUGUCA'))
