dae37e9a57fa675ead9b727f97ff7fac377a5bb1
nazKiyrT207 <100958626+nazKiyrT207@users.noreply.github.com>
2022-03-04 19:42:11 -0600

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -120,19 +120,18 @@ def rna2codon(rna):
 
 
 #MUSKAN10
-
 def count_dom_phenotype(genotypes):
-	output = 0
-	for n in range(0 ,6):
-		if n <= 2:
-        		output += genotypes[n] * 2
-    		elif n == 3:
-        		output += genotypes[n] * 1.5
-    		elif n == 4:
-        		output += genotypes[n]
-    		else :
-        		continue
-	return output
+    output = 0
+    for n in range(0 ,6):
+	if n <= 2:
+	    output += genotypes[n] * 2
+	elif n == 3:
+            output += genotypes[n] * 1.5
+    	elif n == 4:
+            output += genotypes[n]
+    	else :
+            continue
+    return output
 
 
 
@@ -141,24 +140,24 @@ def count_dom_phenotype(genotypes):
 #MUSKAN11
 
 def source_rna(protein):
-	count = 1
-	Dict = {'F' : 2, 'L' : 6, 'S' : 6, 'Y' : 2, 'C' : 2, 'W' : 1, 'P' : 4, 'H' : 2, 'Q' : 2, 'R' : 6, 'I' : 3, 'M' : 1, 'T' : 4,
+    count = 1
+    Dict = {'F' : 2, 'L' : 6, 'S' : 6, 'Y' : 2, 'C' : 2, 'W' : 1, 'P' : 4, 'H' : 2, 'Q' : 2, 'R' : 6, 'I' : 3, 'M' : 1, 'T' : 4,
         	'N' : 2, 'K' : 2, 'V' : 4, 'A' : 4, 'D' : 2, 'E'  :2, 'G' : 4}
-	for a in protein:
-		count = count * Dict[a]
-		count = count * 3
-	return count%1000000
+    for a in protein:
+        count = count * Dict[a]
+	count = count * 3
+    return count%1000000
 
 #print result for sequence "MA"
 
 
 #12
 def splice_rna(dna, intron_list):
-	proteinString = ''
-	for i in intron_list:
-		dna = dna.replace(i, '')
-	rna = dna2rna(dna)
-	proteinString = rna2codon(rna)
-	return proteinString
+    proteinString = ''
+    for i in intron_list:
+	dna = dna.replace(i, '')
+    rna = dna2rna(dna)
+    proteinString = rna2codon(rna)
+    return proteinString
 
 
