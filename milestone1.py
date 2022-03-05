65a4791f72ce253704c95e1ff206765f15920401
nazKiyrT207 <100958626+nazKiyrT207@users.noreply.github.com>
2022-03-04 20:04:50 -0600

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -131,11 +131,10 @@ def count_dom_phenotype(genotypes):
 
 def source_rna(protein):
     count = 1
-    Dict = {'F' : 2, 'L' : 6, 'S' : 6, 'Y' : 2, 'C' : 2, 'W' : 1, 'P' : 4, 'H' : 2, 'Q' : 2, 'R' : 6, 'I' : 3, 'M' : 1, 'T' : 4,
+    dict = {'F' : 2, 'L' : 6, 'S' : 6, 'Y' : 2, 'C' : 2, 'W' : 1, 'P' : 4, 'H' : 2, 'Q' : 2, 'R' : 6, 'I' : 3, 'M' : 1, 'T' : 4,
         	'N' : 2, 'K' : 2, 'V' : 4, 'A' : 4, 'D' : 2, 'E'  :2, 'G' : 4}
     for a in protein:
-        count = count * Dict[a]
-        count = count * 3
+        count = count * dict[a]
     return count%1000000
 #print result for sequence "MA"
 
