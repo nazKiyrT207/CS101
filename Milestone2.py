5a35e17a25596741d0e79f2b3b6e105006dca2f0
nazKiyrT207 <100958626+nazKiyrT207@users.noreply.github.com>
2022-04-10 18:23:49 -0500

    Update Milestone2.py

--- Milestone2.py
+++ Milestone2.py
@@ -1,6 +1,8 @@
 def find_splice(dna_motif, dna):
   start = 0
   li = []
+  print(dna_motif)
+  print(dna)
   for i in dna_motif:
     li.append(dna.index(i, start))
     start = dna.index(i, start+1)
