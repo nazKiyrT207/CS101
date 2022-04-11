48f7683e1bc792c6133dfaecbf1ab289d16ee266
nazKiyrT207 <100958626+nazKiyrT207@users.noreply.github.com>
2022-04-10 19:13:21 -0500

    Update Milestone2.py

--- Milestone2.py
+++ Milestone2.py
@@ -9,3 +9,20 @@ def find_splice(dna_motif, dna):
     li.append(dna.index(i, start))
     start = dna.index(i, start)+1
   return li
+
+def shared_motif(dna_list):
+  firstDna = dna_list[0]
+  l = len(firstDna)
+  lcs = ""
+  for i in range(l):
+    for j in range(i,l):
+      substring = firstDna[i:j+1]
+      flag = True
+      for k in dna_list:
+        if substring not in k:
+          flag = False
+          break
+      if flag:
+        if len(substring) > len(lcs):
+          lcs = substring
+  return lcs
